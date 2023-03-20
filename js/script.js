0aed3303b21895588313c9b964a93e71537e6018
ArsenyKornilcev <76811297+ArsenyKornilcev@users.noreply.github.com>
2023-03-20 17:17:45 +0500

    css, js update

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
 window.addEventListener("DOMContentLoaded", function () {
 	"use strict";
 	const links = document.querySelectorAll('a[href^="#"]');
-	links.forEach(function(link) {
-		link.addEventListener('click', function(e){
+	links.forEach(function (link) {
+		link.addEventListener("click", function (e) {
 			e.preventDefault();
 
-			const id = link.getAttribute('href');
+			const id = link.getAttribute("href");
 			document.querySelector(id).scrollIntoView({
-				'behavior': 'smooth',
-				'block': 'start'
+				behavior: "smooth",
+				block: "start",
 			});
 		});
 	});
-
 
 	let tab = document.querySelectorAll(".info-header-tab");
 	let info = document.querySelector(".info-header");
@@ -175,95 +174,96 @@ window.addEventListener("DOMContentLoaded", function () {
 
 	// Slider
 
-    let slideIndex = 1,
-        slides = document.querySelectorAll('.slider-item'),
-        prev = document.querySelector('.prev'),
-        next = document.querySelector('.next'),
-        dotsWrap = document.querySelector('.slider-dots'),
-        dots = document.querySelectorAll('.dot');
+	let slideIndex = 1,
+		slides = document.querySelectorAll(".slider-item"),
+		prev = document.querySelector(".prev"),
+		next = document.querySelector(".next"),
+		dotsWrap = document.querySelector(".slider-dots"),
+		dots = document.querySelectorAll(".dot");
 
-    showSlides(slideIndex);
+	showSlides(slideIndex);
 
-    function showSlides(n) {
+	function showSlides(n) {
+		if (n > slides.length) {
+			slideIndex = 1;
+		}
+		if (n < 1) {
+			slideIndex = slides.length;
+		}
 
-        if (n > slides.length) {
-            slideIndex = 1;
-        }
-        if (n < 1) {
-            slideIndex = slides.length;
-        }
+		slides.forEach((item) => (item.style.display = "none"));
+		dots.forEach((item) => item.classList.remove("dot-active"));
 
-        slides.forEach((item) => item.style.display = 'none');
-        dots.forEach((item) => item.classList.remove('dot-active'));
+		slides[slideIndex - 1].style.display = "block";
+		dots[slideIndex - 1].classList.add("dot-active");
+	}
 
-        slides[slideIndex - 1].style.display = 'block';
-        dots[slideIndex - 1].classList.add('dot-active');
-    }
+	function plusSlides(n) {
+		showSlides((slideIndex += n));
+	}
+	function currentSlide(n) {
+		showSlides((slideIndex = n));
+	}
 
-    function plusSlides(n) {
-        showSlides(slideIndex += n); 
-    }
-    function currentSlide(n) {
-        showSlides(slideIndex = n);
-    }
+	prev.addEventListener("click", function () {
+		plusSlides(-1);
+	});
 
-    prev.addEventListener('click', function() {
-        plusSlides(-1);
-    });
+	next.addEventListener("click", function () {
+		plusSlides(1);
+	});
 
-    next.addEventListener('click', function() {
-        plusSlides(1);
-    });
-
-    dotsWrap.addEventListener('click', function(event) {
-        for (let i = 0; i <= dots.length; i++) {
-            if (event.target.classList.contains('dot') && event.target == dots[i-1]) {
-                currentSlide(i);
-            }
-        }
-    });
+	dotsWrap.addEventListener("click", function (event) {
+		for (let i = 0; i <= dots.length; i++) {
+			if (
+				event.target.classList.contains("dot") &&
+				event.target == dots[i - 1]
+			) {
+				currentSlide(i);
+			}
+		}
+	});
 
 	// Calc
 
-    let persons = document.querySelectorAll('.counter-block-input')[0],
-        restDays = document.querySelectorAll('.counter-block-input')[1],
-        place = document.getElementById('select'),
-        totalValue = document.getElementById('total'),
-        personsSum = 0,
-        daysSum = 0,
-        total = 0;
+	let persons = document.querySelectorAll(".counter-block-input")[0],
+		restDays = document.querySelectorAll(".counter-block-input")[1],
+		place = document.getElementById("select"),
+		totalValue = document.getElementById("total"),
+		personsSum = 0,
+		daysSum = 0,
+		total = 0;
 
-    totalValue.innerHTML = 0;
+	totalValue.innerHTML = 0;
 
-    persons.addEventListener('change', function() {
-        personsSum = +this.value;
-        total = (daysSum + personsSum)*4000;
+	persons.addEventListener("change", function () {
+		personsSum = +this.value;
+		total = (daysSum + personsSum) * 4000;
 
-        if(restDays.value == '') {
-            totalValue.innerHTML = 0;
-        } else {
-            totalValue.innerHTML = total;
-        }
-    });
+		if (restDays.value == "") {
+			totalValue.innerHTML = 0;
+		} else {
+			totalValue.innerHTML = total;
+		}
+	});
 
-    restDays.addEventListener('change', function() {
-        daysSum = +this.value;
-        total = (daysSum + personsSum)*4000;
+	restDays.addEventListener("change", function () {
+		daysSum = +this.value;
+		total = (daysSum + personsSum) * 4000;
 
-        if(persons.value == '') {
-            totalValue.innerHTML = 0;
-        } else {
-            totalValue.innerHTML = total;
-        }
-    });
+		if (persons.value == "") {
+			totalValue.innerHTML = 0;
+		} else {
+			totalValue.innerHTML = total;
+		}
+	});
 
-    place.addEventListener('change', function() {
-        if (restDays.value == '' || persons.value == '') {
-            totalValue.innerHTML = 0;
-        } else {
-            let a = total;
-            totalValue.innerHTML = a * this.options[this.selectedIndex].value;
-        }
-    });
+	place.addEventListener("change", function () {
+		if (restDays.value == "" || persons.value == "") {
+			totalValue.innerHTML = 0;
+		} else {
+			let a = total;
+			totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+		}
+	});
 });
-
